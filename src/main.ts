62f5791086a2f7aeab86ca747afcfb5c8adb9f1b
skatkar <skatkar225@gmail.com>
2023-04-11 21:25:19 -0500

    Code modifications

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
+import {
+  INestApplication,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = (await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    abortOnError: false, // create a nest module and rethrow an error, instead of aborting the startup
+  }).catch(console.error)) as INestApplication;
 
   app.enableCors();
+  // whitelist ensures that only DTO properties specified in the request body wil be considered
+  // if unknown properties OR properties not defined in the DTO are provided, it'll ignore it.
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); //enables DTO validations
+
+  // enables URI versioning
+  // Reference: https://docs.nestjs.com/techniques/versioning#uri-versioning-type
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
 
   if (process.env.NODE_ENV !== 'production') {
     const config = new DocumentBuilder()
@@ -17,15 +33,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('swagger-ui', app, document);
   }
-  // whitelist ensures that only DTO properties are specified in the request body
-  // if unknown properties/properties not defined in the DTO is provided, it'll ignore it.
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); //enables DTO validations
-
-  // enables URI versioning
-  // Reference: https://docs.nestjs.com/techniques/versioning#uri-versioning-type
-  app.enableVersioning({
-    type: VersioningType.URI,
-  });
 
   await app.listen(3000);
 }
